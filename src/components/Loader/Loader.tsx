121f4226cc80507bd1198c19b6d0e6c2bbc9c36b
ayushgajjar7 <ayushgajjar712@gmail.com>
2026-09-23 21:09:03 +0530

    Update website design and components

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -63,8 +63,8 @@ export function Loader() {
                 className={styles.centerContent}
                 initial={{ opacity: 0, scale: 0.9, x: "-50%", y: "-50%" }}
                 animate={{ opacity: 1, scale: 1, x: "-50%", y: "-50%" }}
-                exit={{ opacity: 0, scale: 1.1, x: "-50%", y: "-50%" }}
-                transition={{ duration: 0.5, ease: "easeInOut" }}
+                exit={{ opacity: 0, scale: 4, x: "-50%", y: "-50%" }}
+                transition={{ duration: 0.8, ease: "easeInOut" }}
               >
                 <div className={styles.logoWrapper}>
                   <Image
